43a7c0f16bf3cc61a01f030e4774822dd5c1025e
정승현 <fm1003@naver.com>
2020-01-17 23:44:14 +0900

    9강

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)#추가될 때에 자동 저장
+    updated_at = models.DateTimeField(auto_now=True)
+
