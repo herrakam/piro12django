528311785e0c271a54b6e133c786394e068df619
정승현 <fm1003@naver.com>
2020-01-19 21:06:59 +0900

    12

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,9 @@ class item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)#추가될 때에 자동 저장
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return '<{}> {}'.format(self.pk, self.name)
