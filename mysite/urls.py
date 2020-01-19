74fb6d0c4cc2311a9e0af32a52f565cb88ab9fac
정승현 <fm1003@naver.com>
2020-01-20 01:35:56 +0900

    16

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog1/', include('blog1.urls')),
     path('shop/', include('shop.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns +=[
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
