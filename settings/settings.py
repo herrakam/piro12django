0f5999a69357609e8caec5a6560bb59b8fa2908e
정승현 <fm1003@naver.com>
2020-01-21 16:34:15 +0900

    31

--- settings/settings.py
+++ settings/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from os.path import dirname, abspath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,9 +129,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'mysite','static'),
+    os.path.join(BASE_DIR, 'mysite', 'static'),
 ]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mysite','media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mysite', 'media')
 INTERNAL_IPS = ['127.0.0.1']
